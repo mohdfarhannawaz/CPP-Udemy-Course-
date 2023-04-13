92471fadcba4bb960c12c4fcbadec1cba210ed28
mohdfarhannawaz <101490489+mohdfarhannawaz@users.noreply.github.com>
2023-04-14 03:17:28 +0530

    Scope rules added to the functions folder

---
+++ Section11/ScopeRules.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int num{5}; // num is 5
+
+int main() {
+    cout << num << endl; // prints 5
+    int num {200}; // num changed to 200
+    cout << num  << endl; // prints 200
+    {
+        cout << num << endl; // prints 200 
+        int num {400}; // num changed to 400
+        cout << num << endl; // prints 400
+    }
+    cout << num << endl; // prints 200
+    static int num1 {1000}; // assigns 1000
+    num1 = (num1 + num1);
+    cout << num1 << endl; // prints 2000
+     {
+        cout << num1 << endl; // prints 1000
+        int num1 {400}; // num changed to 2400
+        cout << num1 << endl; // prints 2400
+    }
+    return 0; 
+
+}

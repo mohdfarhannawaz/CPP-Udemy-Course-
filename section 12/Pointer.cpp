4000ad469abb3b6d8c27a579b0eed335fa73b231
mohdfarhannawaz <101490489+mohdfarhannawaz@users.noreply.github.com>
2023-04-19 17:07:11 +0530

    completed the pointer.cpp file and added some comments in it

--- section 12/Pointer.cpp
+++ section 12/Pointer.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main () {
+    int num{10};
+    cout <<"the value of num : "<< num << endl; // 10
+    cout <<"the sizeof num : "<< sizeof num << endl; // 4 , the other way of displaying sizeof is = sizeof(num)
+                                                     // both ways are fine
+    cout <<"the address of num : "<< &num << endl; // address of the variable, garbage data i.e, 0xa0317ffc6c
     return 0;
 }

22f03bf45f3c13a17c32e907f22f4cca65c0494c
mohdfarhannawaz <101490489+mohdfarhannawaz@users.noreply.github.com>
2023-05-20 10:27:58 +0530

    added comments in the arrayAndPointer.cpp file

--- section 12/ArrayAndPointer.cpp
+++ section 12/ArrayAndPointer.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main () {
     int score [] {100,95,50};
-    cout << "value of score " << score << endl;
-    
-    int *score_ptr {score};
-    cout << "value of score pointer " << score_ptr << endl; 
+    cout << "value of score " << score << endl; //address array is the address of first element of the array
+    // lets assume it to be 1000
+    int *score_ptr {score}; 
+    cout << "value of score pointer " << score_ptr << endl; // same address as the array
 
     cout << "\nArray subscript Notation................." << endl;
-    cout << score[0] << endl;
-    cout << score[1] << endl;
+    cout << score[0] << endl; 
+    cout << score[1] << endl; 
     cout << score[2] << endl;
 
     cout << "\nPointer subscript Notation..............." << endl;
@@ -20,12 +20,12 @@ int main () {
 
     cout << "\nPointer offset notation....................." << endl;
     cout << score_ptr << endl;
-    cout << *(score_ptr + 1) << endl;
+    cout << *(score_ptr + 1) << endl; // address of the array with addition of data type x bit i.e, 4x1 address 
     cout << *(score_ptr + 2) << endl;
      
     cout << "\nArray offset notation........................" << endl;
     cout << *score << endl;
-    cout << *(score + 1) << endl;
+    cout << *(score + 1) << endl; // address of the array with addition of data type x bit i.e, 4x1 address
     cout << *(score + 2) << endl;
 
     cout << endl;
